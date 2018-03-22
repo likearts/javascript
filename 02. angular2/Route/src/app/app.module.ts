3fdc1121fb19bff572abf01885cde3c859ae836d
= <=>
2018-03-22 14:35:14 +0900

    async await

--- 02. angular2/Route/src/app/app.module.ts
+++ 02. angular2/Route/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { routing, appRoutingProviders } from '../route/routes';
@@ -24,7 +25,8 @@ import {
   imports: [
     BrowserModule,
     routing,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    HttpClientModule
   ],
   providers: [appRoutingProviders],
   bootstrap: [AppComponent]
--- 02. angular2/Route/src/component/home/comm.home.ts
+++ 02. angular2/Route/src/component/home/comm.home.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component,  } from '@angular/core';
+import { HttpClient  } from '@angular/common/http';
 import { slideInOutAnimation } from "../../animations/slideInOutAnimation";
 
 @Component({
@@ -10,7 +11,33 @@ import { slideInOutAnimation } from "../../animations/slideInOutAnimation";
 })
 
 export class HomeComponent {
-  constructor(){
+  constructor(private http: HttpClient  ){
     console.log( "HomeComponent", new Date().getTime());
+    console.log('01');
+    console.log('res', this.loader().then(res => console.log('res->res',res)) );
+    console.log('--end');
+
+    var cdata = this.loader2();
+    console.log('cdata', cdata );
+  }
+
+  async loader(): Promise<any> {
+    let data = await(await (fetch('./assets/getKshopMainJson.json')
+      .then(res => {
+        return res.json();
+      })
+      .catch(err => {
+        console.log('Error: ', err)
+      })
+    ))
+    console.log('loader', data);
+
+    return data;
+  }
+
+  async loader2(): Promise<any> {
+    const data = await fetch('./assets/getKshopMainJson.json')
+    console.log('loader2', data);
+    return await data.json();
   }
 }
